7c08c345d1376ef451cb69920c4e8ef4197c6cbd
mohammedjammeh <mohammedjammeh@yahoo.com>
2024-05-28 20:48:33 +0100

    Update weight/create and its styles.

--- resources/views/scale/create.blade.php
+++ resources/views/scale/create.blade.php
@@ -1,5 +1,22 @@
 
 <x-layout>
-    <p>create</p>
+    <div class="create-holder">
+        <form action="#" class="columnContainer" id="linkupForm">
+            <div>
+                <input type="text" name="id" id="id" placeholder="id" />
+            </div>
+
+            <div>
+                <input type="text" name="name" id="name" placeholder="name" />
+            </div>
+
+            <div>
+                <textarea name="description" id="description" placeholder="description" rows="5"></textarea>
+            </div>
+
+            <div>
+                <input type="submit" value="save" />
+            </div>
+        </form>
     </div>
 </x-layout>
